da73ea2ca17cd285329dd8e46a1e64ce1be1e238
Leroy <TheonlyTazz@gmail.com>
2026-02-09 18:10:50 +0100

    disable any Curing of Deus Ex Machina effect and bump version to 1.0.2

--- src/main/java/com/breakinblocks/deus_ex_machina/effect/DeusExMachinaEffect.java
+++ src/main/java/com/breakinblocks/deus_ex_machina/effect/DeusExMachinaEffect.java
@@ -2,9 +2,17 @@ package com.breakinblocks.deus_ex_machina.effect;
 
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DeusExMachinaEffect extends MobEffect {
     public DeusExMachinaEffect(MobEffectCategory category, int color) {
         super(category, color);
     }
+
+    @Override
+    public List<ItemStack> getCurativeItems() { return new ArrayList<>();}
 }
