9a39a0edac4680a6c5561b7e094c10eeda69413f
Leroy <TheonlyTazz@gmail.com>
2026-02-18 18:55:31 +0100

    Refactor Brewing Registry and update Ambrosia recipe
    Change Ambrosia recipe input from Long Regeneration to Strong Healing.
    Refactor BrewingRegistry to track recipes in a list so JEI can
    dynamically display all registered brewing recipes.

--- src/main/java/com/breakinblocks/deus_ex_machina/integration/DeusJEIPlugin.java
+++ src/main/java/com/breakinblocks/deus_ex_machina/integration/DeusJEIPlugin.java
@@ -1,20 +1,19 @@
 package com.breakinblocks.deus_ex_machina.integration;
 
 import com.breakinblocks.deus_ex_machina.DeusExMachina;
-import com.breakinblocks.deus_ex_machina.registry.ItemRegistry;
+import com.breakinblocks.deus_ex_machina.registry.BrewingRegistry;
 import mezz.jei.api.IModPlugin;
 import mezz.jei.api.JeiPlugin;
 import mezz.jei.api.constants.RecipeTypes;
 import mezz.jei.api.recipe.vanilla.IJeiBrewingRecipe;
 import mezz.jei.api.recipe.vanilla.IVanillaRecipeFactory;
 import mezz.jei.api.registration.IRecipeRegistration;
-import net.minecraft.core.component.DataComponents;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.alchemy.PotionContents;
-import net.minecraft.world.item.alchemy.Potions;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @JeiPlugin
@@ -31,17 +30,19 @@ public class DeusJEIPlugin implements IModPlugin {
     public void registerRecipes(IRecipeRegistration registration) {
         IVanillaRecipeFactory factory = registration.getVanillaRecipeFactory();
 
-        // Long Regeneration Potion + Honey Bottle = Ambrosia
-        ItemStack inputPotion = PotionContents.createItemStack(Items.POTION, Potions.LONG_REGENERATION);
-        ItemStack ingredient = new ItemStack(Items.HONEY_BOTTLE);
-        ItemStack output = new ItemStack(ItemRegistry.AMBROSIA.get());
+        List<IJeiBrewingRecipe> brewingRecipes = new ArrayList<>();
+        for (BrewingRegistry.BrewingRecipe recipe : BrewingRegistry.getRecipes()) {
+            ItemStack inputPotion = PotionContents.createItemStack(Items.POTION, recipe.getInputPotion());
+            ItemStack ingredient = new ItemStack(recipe.getIngredientItem());
+            ItemStack output = recipe.getRecipeOutput();
 
-        IJeiBrewingRecipe brewingRecipe = factory.createBrewingRecipe(
-                List.of(ingredient),
-                List.of(inputPotion),
-                output
-        );
+            brewingRecipes.add(factory.createBrewingRecipe(
+                    List.of(ingredient),
+                    List.of(inputPotion),
+                    output
+            ));
+        }
 
-        registration.addRecipes(RecipeTypes.BREWING, List.of(brewingRecipe));
+        registration.addRecipes(RecipeTypes.BREWING, brewingRecipes);
     }
 }
--- src/main/java/com/breakinblocks/deus_ex_machina/registry/BrewingRegistry.java
+++ src/main/java/com/breakinblocks/deus_ex_machina/registry/BrewingRegistry.java
@@ -1,9 +1,12 @@
 package com.breakinblocks.deus_ex_machina.registry;
 
 import com.breakinblocks.deus_ex_machina.DeusExMachina;
+import net.minecraft.core.Holder;
 import net.minecraft.core.component.DataComponents;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.item.alchemy.Potion;
 import net.minecraft.world.item.alchemy.PotionContents;
 import net.minecraft.world.item.alchemy.Potions;
 import net.neoforged.bus.api.SubscribeEvent;
@@ -11,33 +14,74 @@ import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.common.brewing.IBrewingRecipe;
 import net.neoforged.neoforge.event.brewing.RegisterBrewingRecipesEvent;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 @EventBusSubscriber(modid = DeusExMachina.MODID)
 public class BrewingRegistry {
 
+    private static final List<BrewingRecipe> RECIPES = new ArrayList<>();
+
+    public static List<BrewingRecipe> getRecipes() {
+        return Collections.unmodifiableList(RECIPES);
+    }
+
     @SubscribeEvent
     public static void registerRecipes(RegisterBrewingRecipesEvent event) {
-        event.getBuilder().addRecipe(new AmbrosiaBrewingRecipe());
-    }
-}
-
-class AmbrosiaBrewingRecipe implements IBrewingRecipe {
-
-    @Override
-    public boolean isInput(ItemStack input) {
-        PotionContents contents = input.get(DataComponents.POTION_CONTENTS);
-        return contents != null && contents.is(Potions.LONG_REGENERATION);
+        addRecipe(event, new BrewingRecipe(
+                Potions.STRONG_HEALING,
+                Items.HONEY_BOTTLE,
+                new ItemStack(ItemRegistry.AMBROSIA.get())
+        ));
     }
 
-    @Override
-    public boolean isIngredient(ItemStack ingredient) {
-        return ingredient.is(Items.HONEY_BOTTLE);
+    private static void addRecipe(RegisterBrewingRecipesEvent event, BrewingRecipe recipe) {
+        event.getBuilder().addRecipe(recipe);
+        RECIPES.add(recipe);
     }
 
-    @Override
-    public ItemStack getOutput(ItemStack input, ItemStack ingredient) {
-        if (isInput(input) && isIngredient(ingredient)) {
-            return new ItemStack(ItemRegistry.AMBROSIA.get());
+    public static class BrewingRecipe implements IBrewingRecipe {
+
+        private final Holder<Potion> input;
+        private final Item ingredient;
+        private final ItemStack output;
+
+        public BrewingRecipe(Holder<Potion> input, Item ingredient, ItemStack output) {
+            this.input = input;
+            this.ingredient = ingredient;
+            this.output = output;
         }
-        return ItemStack.EMPTY;
+
+        public Holder<Potion> getInputPotion() {
+            return input;
+        }
+
+        public Item getIngredientItem() {
+            return ingredient;
+        }
+
+        public ItemStack getRecipeOutput() {
+            return output.copy();
+        }
+
+        @Override
+        public boolean isInput(ItemStack input) {
+            PotionContents contents = input.get(DataComponents.POTION_CONTENTS);
+            return contents != null && contents.is(this.input);
+        }
+
+        @Override
+        public boolean isIngredient(ItemStack ingredient) {
+            return ingredient.is(this.ingredient);
+        }
+
+        @Override
+        public ItemStack getOutput(ItemStack input, ItemStack ingredient) {
+            if (isInput(input) && isIngredient(ingredient)) {
+                return this.output.copy();
+            }
+            return ItemStack.EMPTY;
+        }
     }
 }
